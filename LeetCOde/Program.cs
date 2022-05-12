e8b87271fbdd2bef432821d85c27b2b9d1ca746e
Bouhadida Brahim Hamza <bouhadidahz95@hotmail.com>
2022-05-12 18:14:56 +0100

    TwoSum

--- LeetCOde/Program.cs
+++ LeetCOde/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCOde;
+
+var easy = new Easy();
 Console.WriteLine("Hello, World!");
+
+foreach (var item in easy.TwoSum(new int[] { 3, 2, 4 }, 6))
+    Console.WriteLine(item);
+
+Console.ReadLine();
+
+
