0b46e85c5ed3c36f1c2ea2455e7d9263cfe14814
Bouhadida Brahim Hamza <bouhadidahz95@hotmail.com>
2022-06-29 00:35:15 +0100

    MaxDepth

--- LeetCOde/Easy.cs
+++ LeetCOde/Easy.cs
@@ -494,6 +494,11 @@ namespace LeetCOde
 
             return (int)last;
         }
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return Math.Max(1 + MaxDepth(root.left), 1 + MaxDepth(root.right));
+        }
 
     }
 
@@ -510,6 +515,19 @@ namespace LeetCOde
         {
             this.val = val;
             this.next = next;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
         }
     }
 }
